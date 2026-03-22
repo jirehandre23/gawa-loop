4ce60e2b4b205ca744e64f395eecef9a9ef3b748
jireh andre <jirehandre121@gmail.com>
2026-03-22 05:34:34 -0400

    Update page.tsx

--- app/business/dashboard/page.tsx
+++ app/business/dashboard/page.tsx
@@ -416,6 +416,8 @@ export default function BusinessDashboardPage() {
                 <option value="15">15 minutes</option>
                 <option value="20">20 minutes</option>
                 <option value="30">30 minutes</option>
+                <option value="45">45 minutes</option>
+                <option value="60">60 minutes</option>
               </select>
             </div>
 
