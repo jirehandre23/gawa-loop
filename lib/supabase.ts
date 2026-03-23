64282565e3f9f484ac2c77811d0f0bb0ece9577c
jireh andre <jirehandre121@gmail.com>
2026-03-22 20:52:34 -0400

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,7 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from "@supabase/supabase-js";
 
-export const supabase = createClient(
-  "https://mytxhilqjqehbnnjlauu.supabase.co",
-  "sb_publishable_8Db3Y5HU60BAcMnMQmAC8Q_xMy5SFPg"
-)
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey =
+  process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY!;
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
